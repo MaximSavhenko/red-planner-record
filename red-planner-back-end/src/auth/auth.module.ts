809b51dddc59c88587fc48ffb64f37fb47c05c34
Maxim <uspmsm@gmail.com>
2025-03-18 18:11:50 +0200

    add auth logic

---
+++ red-planner-back-end/src/auth/auth.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common'
+import { AuthService } from './auth.service'
+import { AuthController } from './auth.controller'
+import { JwtStrategy } from './jwt.strategy'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt'
+import { getJwtConfig } from 'src/config/jwt.config'
+import { UserService } from 'src/user/user.service'
+import { UserModule } from 'src/user/user.module'
+
+@Module({
+	imports: [
+		UserModule,
+		ConfigModule,
+		JwtModule.registerAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getJwtConfig
+		})
+	],
+	controllers: [AuthController],
+	providers: [AuthService, JwtStrategy]
+})
+export class AuthModule {}
---
+++ red-planner-back-end/src/auth/auth.service.ts
@@ -0,0 +1,16 @@
+import { UserService } from 'src/user/user.service'
+import { Injectable } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { AuthDto } from './dto/auth.dto'
+
+@Injectable()
+export class AuthService {
+	constructor(
+		private jwt: JwtService,
+		private UserService: UserService
+	) {}
+
+	async login(dto: AuthDto) {
+		return dto
+	}
+}
