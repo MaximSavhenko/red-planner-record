5dfcbb21a75e8c1710921b8ae7fe498aca9a1781
Maxim <uspmsm@gmail.com>
2025-06-25 15:12:52 +0300

    added register and auth

--- red-planner-back-end/src/auth/auth.service.ts
+++ red-planner-back-end/src/auth/auth.service.ts
@@ -1,7 +1,13 @@
 import { UserService } from 'src/user/user.service'
-import { Injectable } from '@nestjs/common'
+import {
+	BadRequestException,
+	Injectable,
+	NotFoundException,
+	UnauthorizedException
+} from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 import { AuthDto } from './dto/auth.dto'
+import { verify } from 'argon2'
 
 @Injectable()
 export class AuthService {
@@ -11,6 +17,53 @@ export class AuthService {
 	) {}
 
 	async login(dto: AuthDto) {
-		return dto
+		const { password, ...user } = await this.validateUser(dto)
+		const tokens = this.issueTokens(user.id)
+
+		return { user, ...tokens }
+	}
+
+	async register(dto: AuthDto) {
+		const oldUser = await this.UserService.getByEmail(dto.email)
+
+		if (oldUser) {
+			throw new BadRequestException('User already exists')
+		}
+
+		const { password, ...user } = await this.UserService.create(dto)
+
+		const tokens = this.issueTokens(user.id)
+
+		return { user, ...tokens }
+	}
+
+	private issueTokens(userId: string) {
+		const data = { id: userId }
+
+		const accessToken = this.jwt.sign(data, {
+			expiresIn: '1h'
+		})
+
+		const refreshToken = this.jwt.sign(data, {
+			expiresIn: '7d'
+		})
+
+		return { accessToken, refreshToken }
+	}
+
+	private async validateUser(dto: AuthDto) {
+		const user = await this.UserService.getByEmail(dto.email)
+
+		if (!user) {
+			throw new NotFoundException('User not found')
+		}
+
+		const isValid = await verify(user.password, dto.password)
+
+		if (!isValid) {
+			throw new UnauthorizedException('Invalid password')
+		}
+
+		return user
 	}
 }
--- red-planner-back-end/src/user/user.service.ts
+++ red-planner-back-end/src/user/user.service.ts
@@ -1,16 +1,37 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma.service';
+import { Injectable } from '@nestjs/common'
+import { hash } from 'argon2'
+import { retry } from 'rxjs'
+import { AuthDto } from 'src/auth/dto/auth.dto'
+import { PrismaService } from 'src/prisma.service'
 
 @Injectable()
 export class UserService {
-  constructor(private prisma: PrismaService){}
+	constructor(private prisma: PrismaService) {}
 
-  async getByID(id: string) {
-    return this.prisma.user.findUnique({
-      where: { id },
-      include: {
-        tasks: true,
-      },
-    })
-  }
+	async getByID(id: string) {
+		return this.prisma.user.findUnique({
+			where: { id },
+			include: {
+				tasks: true
+			}
+		})
+	}
+
+	getByEmail(email: string) {
+		return this.prisma.user.findUnique({
+			where: { email }
+		})
+	}
+
+	async create(dto: AuthDto) {
+		const user = {
+			email: dto.email,
+			name: '',
+			password: await hash(dto.password)
+		}
+
+		return this.prisma.user.create({
+			data: user
+		})
+	}
 }
