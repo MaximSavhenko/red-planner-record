f94856bf1fbbaa98a255be078259efd48d55f1ca
Maxim <uspmsm@gmail.com>
2025-06-26 16:04:09 +0300

    added profile and statistics back-end

--- red-planner-back-end/src/auth/decorators/user.decorator.ts
+++ red-planner-back-end/src/auth/decorators/user.decorator.ts
@@ -1,10 +1,14 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common'
+import { createParamDecorator, ExecutionContext, UnauthorizedException } from '@nestjs/common'
 import { User } from '@prisma/client'
 
 export const CurrentUser = createParamDecorator(
 	(data: keyof User, ctx: ExecutionContext) => {
 		const request = ctx.switchToHttp().getRequest()
-		const user = request.User
+		const user = request.user
+
+		if (!user) {
+			throw new UnauthorizedException('User not found in request')
+		}
 
 		return data ? user[data] : user
 	}
--- red-planner-back-end/src/user/user.service.ts
+++ red-planner-back-end/src/user/user.service.ts
@@ -3,6 +3,8 @@ import { hash } from 'argon2'
 import { retry } from 'rxjs'
 import { AuthDto } from 'src/auth/dto/auth.dto'
 import { PrismaService } from 'src/prisma.service'
+import { UserDto } from './dto/user.dto'
+import { startOfDay, subDays } from 'date-fns'
 
 @Injectable()
 export class UserService {
@@ -23,6 +25,51 @@ export class UserService {
 		})
 	}
 
+	async getProfile(id: string) {
+		const profile = await this.getByID(id)
+
+		const totalTasks = profile.tasks.length
+		const complitedTasks = await this.prisma.task.count({
+			where: {
+				userId: id,
+				isCompleted: true
+			}
+		})
+
+		const todayStart = startOfDay(new Date())
+		const weekStart = startOfDay(subDays(new Date(), 7))
+
+		const todayTasks = await this.prisma.task.count({
+			where: {
+				userId: id,
+				createdAt: {
+					gte: todayStart.toISOString()
+				}
+			}
+		})
+
+		const weekTasks = await this.prisma.task.count({
+			where: {
+				userId: id,
+				createdAt: {
+					gte: weekStart.toISOString()
+				}
+			}
+		})
+
+		const { password, ...rest } = profile
+
+		return {
+			user: rest,
+			statistics: [
+				{ label: 'Total', value: totalTasks },
+				{ label: 'Completed tasks', value: complitedTasks },
+				{ label: 'Today tasks', value: todayTasks },
+				{ label: 'Week tasks', value: weekTasks }
+			]
+		}
+	}
+
 	async create(dto: AuthDto) {
 		const user = {
 			email: dto.email,
@@ -32,6 +79,24 @@ export class UserService {
 
 		return this.prisma.user.create({
 			data: user
+		})
+	}
+
+	async update(id: string, dto: UserDto) {
+		let data = dto
+		if (dto.password) {
+			data = { ...dto, password: await hash(data.password) }
+		}
+
+		return this.prisma.user.update({
+			where: {
+				id
+			},
+			data,
+			select: {
+				name: true,
+				email: true
+			}
 		})
 	}
 }
