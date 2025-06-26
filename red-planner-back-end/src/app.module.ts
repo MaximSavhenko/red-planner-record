9622b98085465bfbfed1a4f5c17941b3fb582a26
Maxim <uspmsm@gmail.com>
2025-06-26 21:03:29 +0300

    added tasks and time-block back-end

--- red-planner-back-end/src/app.module.ts
+++ red-planner-back-end/src/app.module.ts
@@ -1,10 +1,11 @@
+import { TimeBlockModule } from './time-block/time-block.module';
 import { Module } from '@nestjs/common'
 import { AuthModule } from './auth/auth.module'
 import { UserModule } from './user/user.module'
 import { ConfigModule } from '@nestjs/config'
+import { TaskModule } from './task/task.module'
 
 @Module({
-	imports: [ConfigModule.forRoot(), AuthModule, UserModule],
-
+	imports: [ConfigModule.forRoot(), AuthModule, UserModule, TaskModule, TimeBlockModule]
 })
 export class AppModule {}
