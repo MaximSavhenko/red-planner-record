38cfb5c3801582cdabd3816c1f35a7266391688a
Maxim <uspmsm@gmail.com>
2025-07-03 14:07:04 +0300

    add list/board switch on task page

--- red-planner-front-end/src/app/i/tasks/TasksView.tsx
+++ red-planner-front-end/src/app/i/tasks/TasksView.tsx
@@ -1,7 +1,29 @@
 'use client'
 
+import Loader from '@/components/ui/Loader'
+
+import { useLocalStorage } from '@/hooks/useLocalStorage'
+
 import { ListView } from './list-view/ListView'
+import { SwitcherView } from '@/app/i/tasks/SwitcherView'
+import { KanbanView } from '@/app/i/tasks/kanban-view/KanbanView'
+
+export type TypeView = 'list' | 'kanban'
 
 export function TasksView() {
-	return <ListView />
+	const [type, setType, isLoading] = useLocalStorage<TypeView>({
+		key: 'view-type',
+		defaultValue: 'list'
+	})
+	if (isLoading) return <Loader />
+
+	return (
+		<div>
+			<SwitcherView
+				type={type}
+				setType={setType}
+			/>
+			{type === 'list' ? <ListView /> : <KanbanView />}
+		</div>
+	)
 }
